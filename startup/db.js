887984286da1eaa187eb56ae07db9316102cdb64
Luis <luis.almajano@gmail.com>
2022-11-14 22:21:51 +0100

    Agregado la opcion useFindAndModify:false en startup/db para omitir deprecation warning con mongoose

--- startup/db.js
+++ startup/db.js
@@ -5,7 +5,25 @@ const config = require("config");
 module.exports = function () {
   const db = config.get("db_atlas");
   mongoose
-    .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => logger.log("info", `[STARTUP] Connected to: ${db.slice(0,23) + "<password>" + db.slice(39)}`, {tags: 'startup, MongoDB'}))
-    .catch((err) => logger.log("error", `message - ${err.message}, stack trace - ${err.stack}`, {tags: 'startup, MongoDB'}));
+    .connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    })
+    .then(() =>
+      logger.log(
+        "info",
+        `[STARTUP] Connected to: ${
+          db.slice(0, 23) + "<password>" + db.slice(39)
+        }`,
+        { tags: "startup, MongoDB" }
+      )
+    )
+    .catch((err) =>
+      logger.log(
+        "error",
+        `message - ${err.message}, stack trace - ${err.stack}`,
+        { tags: "startup, MongoDB" }
+      )
+    );
 };
